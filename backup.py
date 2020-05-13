45d83f8095ebe20b2eee47b4396ed56cf8bb98ad
ristep <ristepan@gmail.com>
2020-05-13 19:00:39 +0200

    minor refactoring

--- backup.py
+++ backup.py
@@ -6,8 +6,13 @@ import pipes
 import configparser
 
 
-def dbBackup(dbName, backupfile):
-    dumpFile = pipes.quote(backupfile) + "/" + dbName + ".sql"
+def dbBackup(dbName, backupdir):
+    try:
+        os.stat(BackupDir)
+    except:
+        os.makedirs(BackupDir)
+
+    dumpFile = pipes.quote(backupdir) + "/" + dbName + ".sql"
     DumpCmd = "mysqldump " + "--defaults-extra-file=./backup.cnf " + dbName + " > " + dumpFile
     # print(DumpCmd)
     if os.system(DumpCmd) == 0:
@@ -18,16 +23,8 @@ def dbBackup(dbName, backupfile):
 
 Config = configparser.ConfigParser()
 Config.read('backup.cnf')
-dbList = Config.get('dblist','list').split(',')
-
-# Getting current DateTime to create backup folders like "2020817-123435".
-BackupFile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backup', time.strftime('%Y%m%d-%H%M%S'))
-BackupDir = os.path.abspath(BackupFile)
-
-try:
-    os.stat(BackupDir)
-except:
-    os.makedirs(BackupDir)
+dbList = Config.get('config','dbList').split(',')
+BackupDir = os.path.join( os.path.abspath(Config.get('config','backupDir')), time.strftime('%Y%m%d-%H%M%S'))
 
 for dbName in dbList:
-    dbBackup(dbName, BackupFile)
+    dbBackup(dbName, BackupDir)
